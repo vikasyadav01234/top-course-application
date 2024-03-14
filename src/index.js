adf04ec2de2f69661bd0ae00d9d81e7508ca9752
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-03-14 12:25:43 +0530

    main

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactTostify.css"
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <div>
     <App />
-  </React.StrictMode>
+    <ToastContainer />
+  </div>
 );
