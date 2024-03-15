5617cba9f8abadc421ae44ea7bcd9e539faf988f
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-03-15 14:20:49 +0530

    main

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -3,10 +3,10 @@ import React from "react";
 const Filter = ({filterData}) => {
     return (
         <div>
-      {filterData.map((data, index) => (
-        <button key={index}>{data.title}</button>
-      ))}
-    </div>
+          {filterData.map((data, index) => (
+          <button key={index}>{data.title}</button>
+          ))}
+        </div>
     )
 }
 
