4ea748fd99de33db1549158a72ae188b62b3df0c
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-03-15 11:35:50 +0530

    main

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactTostify.css"
+//import "react-toastify/dist/ReactTostify.css"
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
