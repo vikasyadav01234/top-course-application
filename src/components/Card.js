6658984e80c15f865a0b1f5cc93862a0330d9c1d
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-03-15 21:46:26 +0530

    main

---
+++ src/components/Card.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Card = () => {
+    return (
+        <div>
+            <div>
+                <img src={course.image.url}></img>
+            </div>
+        </div>
+    )
+}
+
+export default Card
