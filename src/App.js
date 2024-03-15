3536aec3deb2bc6964b6d2d2a94af0a4d6fea051
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-03-15 14:32:36 +0530

    main

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
 import { apiUrl,filterData} from "./data";
 import Filter from "./components/Filter";
 import Cards from "./components/Cards";
+import { toast } from "react-toastify";
 const App = () => {
+  
+  const [courses, setCourses] = useState
+  useEffect ( ()=>{
+    //const fetchData = async() =>{
+      try{
+        const res =await fetch(apiUrl);
+        const data = await res.json();
+        //save data into a variable
+        console.log(data);
+      }
+      catch(error){
+        toast.error("Something went wrong");
+      }
+    }
+    fetchData();
+  },[]);
   return (
     <div>
       <Navbar/>
