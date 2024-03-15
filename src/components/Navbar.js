41d7f71b3cb6a8c6a10a009f6315ced30e77767b
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-03-15 11:39:34 +0530

    main

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,11 @@ import React from "react";
 const Navbar = () => {
     return (
         <div>
-            
+            <nav>
+                <h1>Top Courses</h1>
+            </nav>
         </div>
     )
 }
+
+export default Navbar
