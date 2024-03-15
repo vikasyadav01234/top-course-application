5bbf4214b3b1a86ab64ebee631620481dc273333
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-03-15 14:42:15 +0530

    main

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,6 +1,15 @@
 import React from "react";
 
 const Cards = ({courses}) => {
+    let allCourses = [];
+    const getCourses = () => {
+        Object.values(courses).forEach((courseCategory) => {
+            courseCategory.forEach((course) => {
+                allCourses.push(course);
+            })
+        })
+        return allCourses;
+    }
     return (
         <div>
 
